8ad7e60ad7094e390520edbc90f1ab8d9c7883df
missVM <125476856+missVM@users.noreply.github.com>
2023-03-28 09:21:44 -0700

    viwe print_char.c

---
+++ print_char.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_char - Prints a single character to stdout.
+ *
+ * @c: The character to be printed.
+ *
+ * Return: The number of characters printed (always 1).
+ */
+int print_char(char c)
+{
+	putchar(c);
+
+	return (1);
+}
+
